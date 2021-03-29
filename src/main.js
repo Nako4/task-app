42558aedf24524bbda278c5804f9b508c62d7081
Nako4 <papnoatelier@gmail.com>
2021-03-29 19:23:01 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+// ./App.vueファイルをAppとして取り込む
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// AppというパーツをHTMLのid="app"に表示する
+// Appは./App.vueというファイルが取り込まれたもの
 new Vue({
   render: h => h(App),
 }).$mount('#app')
